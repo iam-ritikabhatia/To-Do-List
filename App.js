f62ee23c2d587b9b32f24f5cdc36dfd1834da6ad
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-06-01 19:28:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+// src/App.js
+import React, { useState } from 'react';
+import TodoList from './Components/TodoList';
+import TodoForm from './Components/TodoForm';
+import './App.css';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
+    </div>
+  );
+};
+
+export default App;
